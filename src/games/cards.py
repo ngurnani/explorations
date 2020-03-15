8c190d0ce1694600e23dba32d0d407565b1c8165
ngurnani <ngurnani@gmail.com>
2020-03-14 23:50:39 -0400

    add sort_index and fix __repr__()

--- src/games/cards.py
+++ src/games/cards.py
@@ -3,25 +3,27 @@ cards.py
 
 Allows you to generate a deck of playing cards
 
-# TODO: Fix the __repr__() in Deck class to print cards correctly
 # TODO: Amend to allow generation of rigged cards based on specification
 
 """
 from dataclasses import dataclass, field
 from typing import List
 
+RANKS = "2 3 4 5 6 7 8 9 10 J Q K A".split()
+SUITS = "C D H S".split()
 
-@dataclass
+
+@dataclass(order=True)
 class PlayingCard:
+    sort_index: int = field(init=False, repr=False)
     rank: str
     suit: str
 
+    def __post_init__(self):
+        self.sort_index = RANKS.index(self.rank) * len(SUITS) + SUITS.index(self.suit)
+
     def __str__(self):
         return f"{self.suit}{self.rank}"
-
-
-RANKS = "2 3 4 5 6 7 8 9 10 J Q K A".split()
-SUITS = "C D H S".split()
 
 
 def make_deck():
@@ -31,3 +33,7 @@ def make_deck():
 @dataclass
 class Deck:
     cards: List[PlayingCard] = field(default_factory=make_deck)
+
+    def __repr__(self):
+        cards = ", ".join(f"{c!s}" for c in self.cards)
+        return f"{self.__class__.__name__}({cards})"
